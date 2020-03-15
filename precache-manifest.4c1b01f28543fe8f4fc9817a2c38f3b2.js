59aedbfb3c76d407a60a3d5d305af3fa600c454f
Lektro9 <49228244+Lektro9@users.noreply.github.com>
2020-03-15 10:56:51 +0100

    init repo

---
+++ precache-manifest.4c1b01f28543fe8f4fc9817a2c38f3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8e7720561d9a126d8d01b8ffdae14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "056522da7e82d8bfb13d70bc5f7c21d3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b856a88ed36dc01be2f5",
+    "url": "/static/css/app.e3cdf4cf.css"
+  },
+  {
+    "revision": "a93a04266e46f872912f",
+    "url": "/static/css/chunk-vendors.781b5119.css"
+  },
+  {
+    "revision": "b856a88ed36dc01be2f5",
+    "url": "/static/js/app.b037a46e.js"
+  },
+  {
+    "revision": "a93a04266e46f872912f",
+    "url": "/static/js/chunk-vendors.08f1293c.js"
+  }
+]);
